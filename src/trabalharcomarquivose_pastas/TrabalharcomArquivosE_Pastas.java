79bba0edebd608a6f28ac2352716903405dadce6
FaustinoPaulo <faustinomuetunda@gmail.com>
2020-09-17 17:29:56 +0100

    Adicionei as Classes FileReader e a BufferReader e posterior mente, tratei com os blocos Try with resources

--- src/trabalharcomarquivose_pastas/TrabalharcomArquivosE_Pastas.java
+++ src/trabalharcomarquivose_pastas/TrabalharcomArquivosE_Pastas.java
@@ -5,6 +5,12 @@
  */
 package trabalharcomarquivose_pastas;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author faust
@@ -16,6 +22,20 @@ public class TrabalharcomArquivosE_Pastas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String path = "D:\\OneDrive\\OneDrive - Universidade da Beira Interior\\UDEMY\\POO\\trabalharcomArquivosE_Pastas\\src\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
-    
+
 }
